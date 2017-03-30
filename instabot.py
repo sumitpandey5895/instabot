b403d22cdb10c73f13f1906778593bfaa462e6c5
sumitpandey5895 <sumit.pandey5895@gmail.com>
2017-03-31 03:18:25 +0530

    Fetching recent posts of the user and reading post id & likes

--- instabot.py
+++ instabot.py
@@ -17,7 +17,7 @@ def self_info():
 #------------------------------------------------------------------------------------
 
 def get_user_by_username():
-    user_list = ["singhsudanshu",]
+    user_list = ["singhsudanshu"]
     user_name = raw_input("Enter Username you wants to search or Q to quit : ")
     if user_name not in user_list:
         if user_name == "q" or user_name == "Q":
@@ -28,10 +28,23 @@ def get_user_by_username():
     else:
         requests_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (user_name, APP_ACCESS_TOKEN)
         user_info = requests.get(requests_url).json()
-        print user_info
-        get_user_by_username()
 
+        print "User id : " + str(user_info['data'][0]['id'])
+        print "Full name : " + str(user_info['data'][0]['full_name'])
+        print "Bio : " + str(user_info['data'][0]['bio'])
+        response = raw_input("Enter Y to perform operations or N to continue : ").upper()
+        if response == "Y":
+            operations(user_info['data'][0]['id'])
+        else:
+            get_user_by_username()
 
-#self_info()
+#-------------------------------------------------------------------------------------------------------------------
+def operations(user_id):
+    requests_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    recent_posts =requests.get(requests_url).json()
+
+    for likes in recent_posts['data']:
+            print "Post id: " + str(likes['id']) + " likes :" + str(likes['likes']['count'])
+
 
 get_user_by_username()
