b78c54b3f593ba2276d22f9a3d279dcf6a132034
sumitpandey5895 <sumit.pandey5895@gmail.com>
2017-04-02 17:40:48 +0530

    Easy to select post now.

--- instabot.py
+++ instabot.py
@@ -42,32 +42,33 @@ def get_user_by_username():
 def operations(user_id):
     requests_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
     recent_posts =requests.get(requests_url).json()
-    post_list = []
+    post_list = ["x"]
     for likes in recent_posts['data']:
             post_list.append(likes['id'])
-            print "User_id : " + str(user_id) + " Post id: " + str(likes['id']) + " likes :" + str(likes['likes']['count'])
-    post_id = raw_input("Enter Post Id you wan't to access or B to go back : ")
-    if post_id not in post_list:
-        if post_id == "b" or "B":
-            get_user_by_username()
-        else:
-            print "Invalid Post id !"
-            operations(user_id)
+            print "User_id : " + str(user_id) + " Post id: " + str(post_list.index(likes['id'])) + " likes :" + str(likes['likes']['count'])
+
+    post_id = raw_input("Enter Post Id you wan't to access OR B to go back: ")
+    if post_id == "b" or post_id == "B":
+        get_user_by_username()
     else:
+        x = int(post_id)
+
+    if post_list[x] not in post_list:
+        print "Invalid Post id !"
+        operations(user_id)
+    else:
+        post_id = post_list[x]
         select_operation(user_id ,post_id)
 
 #_____________________________________________________________________________________________
 def select_operation(user_id , post_id):
-    opr = raw_input("Enter L to like a Post OR C to comment on a Post OR b to go back : ").upper()
+    opr = raw_input("Enter L to like a Post OR C to comment on a Post OR B to go back : ").upper()
     if opr == "L":
         like_post(user_id , post_id)
     elif opr == "C":
         comment_post(user_id , post_id)
-    elif opr == "b":
+    elif opr == "B":
         operations(user_id)
-
-
-
 #_____________________________________________________________________________________________
 def like_post(uid , post_id):
     payload = {'access_token':APP_ACCESS_TOKEN}
